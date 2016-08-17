d230b7b4c924cb9b267b929b28434a6e7180648f
i52gagrj <i52gagrj@uco.es>
2016-08-17 19:02:56 +0200

    Retorno20160817

--- app/app.js
+++ app/app.js
@@ -23,8 +23,8 @@ var Frontend=angular.module('Frontend', [
 Frontend.constant('CONFIG', {
         //APIURL: "http://192.168.1.3/TPVCJ1.2/web/app.php/caja/datos",
         //APIURL2: "http://192.168.1.3/TPVCJ1.2/web/app.php/caja/gestion",
-	APIURL: "http://ancient-mesa-9870.herokuapp.com/caja/datos",
-        APIURL2: "http://ancient-mesa-9870.herokuapp.com/caja/gestion",  
+	APIURL: "https://ancient-mesa-9870.herokuapp.com/caja/datos",
+        APIURL2: "https://ancient-mesa-9870.herokuapp.com/caja/gestion",  
 })
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/venta'});
