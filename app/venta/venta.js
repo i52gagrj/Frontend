8855cc77302647d788e4e63cbfa7d6303d44fa51
i52gagrj <i52gagrj@uco.es>
2016-09-05 20:07:52 +0200

    20160905-2007

--- app/venta/venta.js
+++ app/venta/venta.js
@@ -11,6 +11,7 @@ angular.module('Frontend.Venta', ['ngRoute','angular-jwt','angular-storage'])
 
 .controller('VentaController', function($scope, store, jwtHelper, $http, backendAPIservice, $location, $window) {
     $scope.terminado=false;
+    $scope.cerrado=false;
     $scope.contado=true;
     $scope.cliente=1;
     $scope.base21=0.00;
