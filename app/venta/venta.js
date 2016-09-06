f0e3c15e82a5512bd66f5d8e2cc1db7be9fa285d
i52gagrj <i52gagrj@uco.es>
2016-09-06 07:34:21 +0200

    20160906-0734

--- app/venta/venta.js
+++ app/venta/venta.js
@@ -11,6 +11,7 @@ angular.module('Frontend.Venta', ['ngRoute','angular-jwt','angular-storage'])
 
 .controller('VentaController', function($scope, store, jwtHelper, $http, backendAPIservice, $location, $window) {
     $scope.terminado=false;
+    $scope.cerrado=false;
     $scope.contado=true;
     $scope.cliente=1;
     $scope.base21=0.00;
@@ -24,10 +25,12 @@ angular.module('Frontend.Venta', ['ngRoute','angular-jwt','angular-storage'])
     $scope.respuesta;
     $scope.error; 
     $scope.codigo;
+    $scope.codigo2;
     $scope.cero=0;  
     $scope.prueba;
     $scope.activo;
     $scope.numventa;
+    $scope.socio;
 
     $scope.listaVenta = [];
     $scope.listaProducto = [];
@@ -39,11 +42,23 @@ angular.module('Frontend.Venta', ['ngRoute','angular-jwt','angular-storage'])
       if(!jwtHelper.isTokenExpired(token))
       {     
         backendAPIservice.getProductos().success(function (recibe) {       
-          var productosArray = recibe.response.productos;
-          console.log(JSON.stringify(productosArray));  
-          $scope.listaProducto = productosArray;
+          var productosArray = recibe.response.productos;            
           store.set('token',recibe.response.token);
-          
+          var codigo2=recibe.code;
+          console.log(JSON.stringify(codigo2));
+          $scope.codigo2=codigo2
+          if($scope.codigo2==0) 
+          {  
+            console.log(JSON.stringify(productosArray));  
+            $scope.listaProducto = productosArray;            
+          }  
+          else
+          {  
+            $scope.cerrado=true;
+            var respuesta1 = recibe.response.respuesta;
+            console.log(JSON.stringify(respuesta1));
+            $scope.respuesta = respuesta1;              
+          }
         });
 
         backendAPIservice.getClientes().success(function (recibe) {        
@@ -90,6 +105,19 @@ angular.module('Frontend.Venta', ['ngRoute','angular-jwt','angular-storage'])
       }
       $scope.Totales();
     };
+
+    /*$scope.CambiarCliente=function(index) {
+      var elemento=0;
+      for(elemento in $scope.listaSocio) {
+        if($scope.listaSocio[elemento].id == index){
+          $scope.socio.nombre = $scope.listaSocio[elemento].nombre;
+          $scope.socio.dni = $scope.listaSocio[elemento].dni;
+          $scope.socio.direccion = $scope.listaSocio[elemento].direccion;
+          $scope.socio.poblacion = $scope.listaSocio[elemento].poblacion;          
+          $scope.socio.saldo = $scope.listaSocio[elemento].saldo;          
+        }    
+      }
+    };*/    
 
     $scope.clienteElegido=function(value) {
       if($scope.cliente==1){ $scope.contado=true; }
@@ -138,7 +166,7 @@ angular.module('Frontend.Venta', ['ngRoute','angular-jwt','angular-storage'])
         console.log(JSON.stringify(numventa));
         $scope.respuesta = respuesta1;
         $scope.numventa = numventa;
-        if(recibe.code==0) store.set('token',recibe.response.token);
+        store.set('token',recibe.response.token);
         $scope.terminado=true;
       });
     };
@@ -158,6 +186,7 @@ angular.module('Frontend.Venta', ['ngRoute','angular-jwt','angular-storage'])
       })*/
       $scope.terminado=false;
       $scope.contado=true;
+      $scope.cerrado=false;
       $scope.cliente=1;
       $scope.base21=0.00;
       $scope.iva21=0.00;
@@ -184,11 +213,20 @@ angular.module('Frontend.Venta', ['ngRoute','angular-jwt','angular-storage'])
         if(!jwtHelper.isTokenExpired(token))
         {     
           backendAPIservice.getProductos().success(function (recibe) {       
-            var productosArray = recibe.response.productos;
-            console.log(JSON.stringify(productosArray));  
-            $scope.listaProducto = productosArray;
+            var productosArray = recibe.response.productos;            
             store.set('token',recibe.response.token);
-            
+            if(recibe.code!=0) 
+            {  
+              $scope.cerrado=true;
+              var respuesta1 = recibe.response.respuesta;
+              console.log(JSON.stringify(respuesta1));
+              $scope.respuesta = respuesta1;
+            }  
+            else
+            {  
+              console.log(JSON.stringify(productosArray));  
+              $scope.listaProducto = productosArray;              
+            }
           });
 
           backendAPIservice.getClientes().success(function (recibe) {        
@@ -228,15 +266,15 @@ angular.module('Frontend.Venta', ['ngRoute','angular-jwt','angular-storage'])
       document.body.innerHTML = originalContents;
     };*/
 
-    /*$scope.printDiv = function(divName) {
+    $scope.printDiv = function(divName) {
       var printContents = document.getElementById(divName).innerHTML;
       var popupWin = window.open('', '_blank', 'width=300,height=300');
       popupWin.document.open();
       popupWin.document.write('<html><head><link rel="stylesheet" type="text/css" href="app.css" /></head><body onload="window.print()">' + printContents + '</body></html>');
       popupWin.document.close();
-    };*/
+    };
 
-    $scope.printDiv = function (divName) {
+    /*$scope.printDiv = function (divName) {
 
       var printContents = document.getElementById(divName).innerHTML;
       var originalContents = document.body.innerHTML;      
@@ -264,7 +302,7 @@ angular.module('Frontend.Venta', ['ngRoute','angular-jwt','angular-storage'])
       popupWin.document.close();
 
       return true;
-    };
+    };*/
 
 
 
